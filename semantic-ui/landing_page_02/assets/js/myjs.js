6022270db4a3d2e50e78279091aa5ed336044da2
gl_zwp <gl_zwp@hotmail.com>
2017-02-05 00:12:46 +0800

    Landing_Page_02

---
+++ semantic-ui/landing_page_02/assets/js/myjs.js
@@ -0,0 +1,50 @@
+﻿//dropdown
+$('.ui.dropdown').dropdown();
+
+//sidebar
+$('.ui.sidebar').sidebar('attach events', '.toc.item').sidebar('setting', 'transition', 'overlay');
+
+var city_location = [
+    {
+        title: 'Boston',
+        description:'Massachusetts'
+    },
+    {
+        title: 'Hartford',
+        description: 'Massachusetts'
+    },
+    {
+        title: 'Denver',
+        description: 'Colorado'
+    },
+    {
+        title: 'Manhattan',
+        description: 'New York'
+    },
+    {
+        title: 'Queens',
+        description: 'New York'
+    },
+    {
+        title: 'Brooklyn',
+        description: 'New York'
+    },
+    {
+        title: 'Bronx',
+        description: 'New York'
+    },
+    {
+        title: 'Cleverland',
+        description: 'Ohio'
+    },
+    {
+        title: 'Worcester',
+        description: 'Massachusetts'
+    }
+];
+
+$('.ui.search.location').search({
+    source: city_location,
+    searchFields: ['title', 'description'],
+    searchFullText: false
+});
